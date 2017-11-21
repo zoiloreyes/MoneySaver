6c8bc5d25ef7151cd43f05506c4fa2b50d491370
Zoilo Ismael Reyes Rodríguez <zoiloismaelreyes1@gmail.com>
2017-11-20 23:51:27 -0400

    Cuenta Banco y Banco Funcional

--- FinanzasPersonales/App_Start/BundleConfig.cs
+++ FinanzasPersonales/App_Start/BundleConfig.cs
@@ -30,6 +30,10 @@ namespace FinanzasPersonales
                       "~/Content/materialize.css",
                       "~/Content/site.css",
                       "~/Content/animate.css"));
+            bundles.Add(new StyleBundle("~/Content/plugincss").Include(
+                      "~/Content/css/select2.min.css"));
+            bundles.Add(new ScriptBundle("~/bundles/pluginjs").Include(
+               "~/Scripts/select2.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
                 "~/Scripts/materialize.js"));
