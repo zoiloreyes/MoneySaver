a8ec320b6d4bf729ea7d58b3d742b700dfd487cb
Zoilo Ismael Reyes Rodríguez <zoiloismaelreyes1@gmail.com>
2017-12-01 22:11:21 -0400

    Resumen en desarrollo

--- FinanzasPersonales/Controllers/HomeController.cs
+++ FinanzasPersonales/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
-﻿using System;
+﻿using FinanzasPersonales.Models;
+using FinanzasPersonales.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Globalization;
 
 namespace FinanzasPersonales.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult GetResumenHTML()
+        {
+            ViewBag.Mes = DateTime.Now.ToString("MMMM", new CultureInfo("es-ES"));
+            var id = Int32.Parse(User.Identity.GetMoneySaverUserID());
+            var usuario = db.Users1.Find(Int32.Parse(User.Identity.GetMoneySaverUserID()));
+            return PartialView(usuario);
+
         }
 
         public ActionResult About()
--- FinanzasPersonales/Scripts/PartialCallbacks.js
+++ FinanzasPersonales/Scripts/PartialCallbacks.js
@@ -1,4 +1,47 @@
-﻿function javascriptContacto() {
+﻿
+function javascriptResumen(){
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+                "Octubre", "Noviembre", "Diciembre"],
+            datasets: [{
+                label: "My First dataset",
+                backgroundColor: 'rgb(137, 191, 67)',
+                borderColor: 'rgb(225,225,225)',
+                data: [8, 10, 5, 2, 20, 30, 45, 50, 71, 60, 30, 44],
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+
+    var ctx = document.getElementById('myChartMes').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'doughnut',
+
+        // The data for our dataset
+        data: {
+            labels: ["Comida", "Ropa", "Cine"],
+            datasets: [{
+                label: "My First dataset",
+                backgroundColor: ['rgb(137, 191, 67)', 'rgb(235, 52, 52)', 'rgb(41, 128, 185)'],
+                borderColor: 'rgb(255, 255, 255)',
+                data: [8, 10, 5,],
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+}
+function javascriptContacto() {
     $("#Agenda #txtBuscar").on("change paste keyup", function () {
         var patron = $(this).val();
         $("#Agenda").find(".contacto").each(function () {
